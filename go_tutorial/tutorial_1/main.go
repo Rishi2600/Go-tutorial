a597bf7493d2e1535488b55b5d6710184a00fbf0
Rishi2600 <rishiraj6177@gmail.com>
2025-06-03 20:40:50 +0530

    var: string

--- go_tutorial/tutorial_1/main.go
+++ go_tutorial/tutorial_1/main.go
@@ -7,6 +7,21 @@ func main() {
 	intNum = intNum + 1
 	fmt.Println(intNum)
 
+	var intNum1 int8 = 56
+	var intNum2 int16 = 66
+
+	var finalInt1 = intNum1 + int8(intNum2)
+	fmt.Println(finalInt1)
+
 	var floatNum float64 = 56.5486589
 	fmt.Println(floatNum)
+
+	var intNum3 int = 522
+	var floatNum3 float64 = 56.451
+
+	var finalInt2 = float64(intNum3) + floatNum3
+	fmt.Println(finalInt2)
+
+	var myString string = "this is a string"
+	fmt.Println(myString)
 }
