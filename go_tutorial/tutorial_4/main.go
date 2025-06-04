a96fdb0c457d3ef3fd0e47febc30cd76f6bc0b03
Rishi2600 <rishiraj6177@gmail.com>
2025-06-04 19:24:01 +0530

    arrays

---
+++ go_tutorial/tutorial_4/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var intArr [3]int32 = [3]int32{1, 2, 3}
+	intArr2 := [3]int32{4, 4, 4} /*short hand to declare an array*/
+	intArr3 := []int32{5, 6, 7}
+	intArr[0] = 5
+
+	fmt.Println(intArr2, intArr3)
+	fmt.Println(intArr[0])
+	fmt.Println(intArr[0:2])
+
+	fmt.Println(&intArr[0])
+	fmt.Println(&intArr[1])
+	fmt.Println(&intArr[2])
+}
+
+/*Array in Go has fixed length, Same type, indexable and contiguous memory.*/
+/*in JavaScript, we can do "const mixedArray = [1, "string", {key: "value"}, function() {}]", but not in Go*/
+/*in typescript, the approach is as same as Go, but can be managed accordingly "const arr: (string | number)[] = ['a', 1]"*/
+/*Error: Argument of type '{ name: string; }' is not assignable to parameter of type 'string | number'*/
