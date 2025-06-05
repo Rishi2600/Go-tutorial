92556f889537584e6e1235063ad09cb1b6efc272
Rishi2600 <rishiraj6177@gmail.com>
2025-06-05 22:55:53 +0530

    structs

---
+++ go_tutorial/tutorial_6/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type object struct {
+	name string
+	age  uint8
+}
+
+func (e object) details() string {
+	fmt.Print(e.name, e.age)
+	return e.name
+}
+
+func main() {
+	var user object = object{name: "rishi", age: 23}
+	user.name = "cristiano"
+	fmt.Printf("user name: %v & user age: %v \n", user.name, user.age)
+
+	user.details()
+}
+
+/*type struct and interface in go is very similar to that of typescript*/
+/*16 line -- {name: "rishi", age: 23} can also be written directly as {"rishi", 23}*/
