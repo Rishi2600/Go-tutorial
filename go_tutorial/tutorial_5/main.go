8ada96e19ffb0b2f4e279d769bae5c9fac493458
Rishi <rishiraj6177@gmail.com>
2025-06-15 23:15:27 +0530

    rune: updated

--- go_tutorial/tutorial_5/main.go
+++ go_tutorial/tutorial_5/main.go
@@ -8,3 +8,6 @@ func main() {
 	fmt.Println(myString)
 	fmt.Println(indexed)
 }
+
+/*rune is a data type that represents a Unicode code point*/
+/*It stores a 32-bit integer value that corresponds to a specific character in the Unicode standard*/
