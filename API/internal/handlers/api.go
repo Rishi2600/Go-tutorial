23b10a7dfab87f17e2aa0fdaabb53a06f0c65a24
Rishi2600 <rishiraj6177@gmail.com>
2025-06-17 22:38:39 +0530

    func Handler which contains route handlers is created

--- API/internal/handlers/api.go
+++ API/internal/handlers/api.go
@@ -1,9 +1,23 @@
 package handlers
 
 import (
+	"github.com/Rishi2600/Go-tutorial/internal/middleware"
 	"github.com/go-chi/chi"
 	chimiddle "github.com/go-chi/chi/middleware"
-	"github.com/Rishi2600/Go-tutorial/internal/handlers"
 )
 
-f
+func Handler(r *chi.Mux) {
+
+	//Global Middleware to ignore slashes
+	r.Use(chimiddle.StripSlashes)
+
+	r.Route("/account", func(router chi.Router) {
+
+		//Middleware for /account route --authorization
+		router.Use(middleware.Authorization)
+
+		router.Get("/coins", GetCoinBalance)
+	})
+}
+
+/*If a func name starts with a capital letter, it can be imported to other packages else it is a private func*/
