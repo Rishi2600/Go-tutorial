dd5c137f043f5b5d6a542df04a2f952ccf4ed75a
Rishi2600 <rishiraj6177@gmail.com>
2025-06-17 23:29:34 +0530

    common func for error respone created

--- API/api/api.go
+++ API/api/api.go
@@ -1,18 +1,18 @@
 package api
 
-// import (
-// 	"encoding/json"
-// 	"net/http"
-// )
+import (
+	"encoding/json"
+	"net/http"
+)
 
 /*few structs to represent parameters for endpoints and responses*/
 
-//coin balance params
+// coin balance params
 type CoinBalanceParams struct {
 	Username string
 }
 
-//coin balance response
+// coin balance response
 type CoinBalanceResponse struct {
 	//status code
 	Code int
@@ -21,7 +21,7 @@ type CoinBalanceResponse struct {
 	Balance int64
 }
 
-//error response
+// error response
 type Error struct {
 	//status code
 	Code int
@@ -29,3 +29,24 @@ type Error struct {
 	//error message
 	Message string
 }
+
+func WriteError(w http.ResponseWriter, message string, code int) {
+	resp := Error{
+		Code:    code,
+		Message: message,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+var (
+	RequestErrorHanlder = func(w http.ResponseWriter, err error) {
+		WriteError(w, err.Error(), http.StatusBadRequest)
+	}
+	InternalErrorHandler = func(w http.ResponseWriter) {
+		WriteError(w, "An unexpected error occured.", http.StatusInternalServerError)
+	}
+)
