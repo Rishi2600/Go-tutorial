2efe4332f9df2e5ef61659898d98b5ec4b010bdd
Rishi2600 <rishiraj6177@gmail.com>
2025-06-24 23:53:13 +0530

    loops: break

--- tutorials/6-loops/main.go
+++ tutorials/6-loops/main.go
@@ -6,11 +6,12 @@ func main() {
 	var value = 0
 	for i := range 10 {
 		if i%2 == 0 {
-			continue
+			break
 		}
 		value += i
+		fmt.Println(value)
 	}
-	fmt.Println(value)
+	fmt.Println("Hello, outside of the loop")
 }
 
 /*i:= range 10 means i := 0; i < 10; i++*/
