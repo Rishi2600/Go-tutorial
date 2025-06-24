d14c874fc386fc7faa09beea2c286996d6ffa80d
Rishi2600 <rishiraj6177@gmail.com>
2025-06-24 22:38:39 +0530

    range syntax in loops

--- tutorials/6-loops/main.go
+++ tutorials/6-loops/main.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var sum int = 0
+	var sum1 int = 0
 	for i := 0; i < 10; i++ {
-		sum += i
+		sum1 += i //sum = sum + i
 	}
 
-	fmt.Println(sum)
+	var sum2 int = 0
+	for j := range 10 {
+		sum2 += j
+	}
+
+	fmt.Println(sum1, sum2)
 }
