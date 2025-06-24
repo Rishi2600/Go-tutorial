4986d578722bdc7721247fd3a03146050c07ac21
Rishi2600 <rishiraj6177@gmail.com>
2025-06-25 00:19:24 +0530

    array: initialized an empty int array

---
+++ tutorials/7-slices/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var array [5]int
+
+	fmt.Println(array)
+}
+
+/*Size of arrays in Go has a predefined size and very strictly typed type whereas in JS, the size of array is dynamic.*/
