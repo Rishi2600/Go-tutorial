25a75f0d8db239a49a19fc037973a3b3577ed024
Rishi2600 <rishiraj6177@gmail.com>
2025-06-25 22:56:08 +0530

    slice: creating an empty slice with no value

--- tutorials/7-slices/main.go
+++ tutorials/7-slices/main.go
@@ -3,23 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var array [5]int = [5]int{1, 2, 3, 4, 5}
 
-	var value = 0
+	var emptySlice []int32
+	fmt.Println(emptySlice)
 
-	for i := range array {
-		value += array[i]
-	}
-
-	fmt.Println(value)
-	fmt.Println(array)
-
-	var maze [3][3]int = [3][3]int{
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9},
-	}
-	fmt.Println(maze)
 }
 
-/*Size of arrays in Go has a predefined size and very strictly typed type whereas in JS, the size of array is dynamic.*/
+/*slice is a flexible, dynamically-sized abstraction over an array.*/
