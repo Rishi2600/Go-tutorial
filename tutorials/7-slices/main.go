c52640c3f51b45041d6cd8cc00c64a544087d595
Rishi2600 <rishiraj6177@gmail.com>
2025-06-25 23:12:03 +0530

    immutability of an array/slice

--- tutorials/7-slices/main.go
+++ tutorials/7-slices/main.go
@@ -5,10 +5,12 @@ import "fmt"
 func main() {
 
 	var array [5]int = [5]int{1, 2, 3, 4, 5}
+	fmt.Println(array)
 
 	var slice = array[1:4]
+	fmt.Println(slice)
 
-	fmt.Println(array)
+	slice = []int{5, 6, 7, 8}
 	fmt.Println(slice)
 
 }
