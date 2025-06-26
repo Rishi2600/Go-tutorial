f46b1191d92953adfe9a35f02705a2d144488a22
Rishi2600 <rishiraj6177@gmail.com>
2025-06-26 19:27:24 +0530

    maps: delete a key-value pair

--- tutorials/8-maps/main.go
+++ tutorials/8-maps/main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var m map[string]int = map[string]int{
-		"age":   99,
-		"numbe": 7,
+	var age map[string]int = map[string]int{
+		"rishi":     23,
+		"cristiano": 40,
+		"grandma":   56,
+		"gojo":      35,
+		"naruto":    27,
 	}
-	fmt.Println(m)
+	fmt.Println(age)
+
+	delete(age, "grandma")
+	fmt.Println(age)
 }
