12b31803a605ad129bc2e44d8af2f1293a7d4448
Rishi2600 <rishiraj6177@gmail.com>
2025-06-28 18:15:31 +0530

    function: a func can return multiple values

--- tutorials/2-functions/main.go
+++ tutorials/2-functions/main.go
@@ -9,6 +9,9 @@ func main() {
 	fmt.Println(concat("Hello", " world"))
 	fmt.Println(concat("Hey", " there!"))
 	fmt.Println(concat("whats", " up!"))
+
+	add, sub := calculator(1, 2)
+	fmt.Printf("add: %v & sub: %v \n", add, sub)
 }
 
 func add(a int, b int) int {
@@ -17,4 +20,8 @@ func add(a int, b int) int {
 
 func concat(first string, second string) string {
 	return first + second
+}
+
+func calculator(a, b int) (int, int) {
+	return a + b, a - b
 }
