82ed0105e1f2f4e2f87b73a8b2842e5984048445
Rishi2600 <rishiraj6177@gmail.com>
2025-06-28 22:06:13 +0530

    1st class func: return a func

--- tutorials/9-advFunc/main.go
+++ tutorials/9-advFunc/main.go
@@ -7,6 +7,12 @@ func main() {
 	result2 := calculator(25, 69, sub)
 
 	fmt.Println(result1, result2)
+
+	double := multiplier(5)
+	triple := multiplier(3)
+
+	fmt.Println(double(3))
+	fmt.Println(triple(3))
 }
 
 func sum(a int, b int) int {
@@ -19,4 +25,10 @@ func sub(a int, b int) int {
 
 func calculator(a int, b int, fn func(int, int) int) int {
 	return fn(a, b)
+}
+
+func multiplier(number int) func(int) int {
+	return func(a int) int {
+		return a * number
+	}
 }
