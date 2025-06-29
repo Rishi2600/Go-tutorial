0ab78ade8fe128ded61cf4df24bca628db553409
Rishi2600 <rishiraj6177@gmail.com>
2025-06-29 20:25:35 +0530

    func currying: func as arg and returns a func

--- tutorials/9-advFunc/main.go
+++ tutorials/9-advFunc/main.go
@@ -2,50 +2,72 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	result1 := calculator(89, 24, sum)
+// 	result2 := calculator(25, 69, sub)
+
+// 	fmt.Println(result1, result2)
+
+// 	double := multiplier(5)
+// 	triple := multiplier(3)
+
+// 	fmt.Println(double(3))
+// 	fmt.Println(triple(3))
+
+// 	numbers := []int{1, 2, 3, 4}
+// 	doubled := mapInts(double, numbers)
+
+// 	fmt.Println(doubled)
+// }
+
+// func sum(a int, b int) int {
+// 	return a + b
+// }
+
+// func sub(a int, b int) int {
+// 	return a - b
+// }
+
+// func calculator(a int, b int, fn func(int, int) int) int {
+// 	return fn(a, b)
+// }
+
+// func multiplier(number int) func(int) int {
+// 	return func(a int) int {
+// 		return a * number
+// 	}
+// }
+
+// func mapInts(f func(int) int, nums []int) []int {
+// 	result := make([]int, len(nums))
+// 	for i, v := range nums {
+// 		result[i] = f(v)
+// 	}
+// 	return result
+// }
+
+// func double(x int) int {
+// 	return x * 2
+// }
+
 func main() {
-	result1 := calculator(89, 24, sum)
-	result2 := calculator(25, 69, sub)
+	multiply := calculator(multiply)
+	add := calculator(add)
 
-	fmt.Println(result1, result2)
-
-	double := multiplier(5)
-	triple := multiplier(3)
-
-	fmt.Println(double(3))
-	fmt.Println(triple(3))
-
-	numbers := []int{1, 2, 3, 4}
-	doubled := mapInts(double, numbers)
-
-	fmt.Println(doubled)
+	fmt.Println(multiply(5))
+	fmt.Println(add(5))
 }
 
-func sum(a int, b int) int {
+func add(a int, b int) int {
 	return a + b
 }
 
-func sub(a int, b int) int {
-	return a - b
+func multiply(a int, b int) int {
+	return a * b
 }
 
-func calculator(a int, b int, fn func(int, int) int) int {
-	return fn(a, b)
-}
-
-func multiplier(number int) func(int) int {
-	return func(a int) int {
-		return a * number
+func calculator(mathFunc func(int, int) int) func(int) int {
+	return func(x int) int {
+		return mathFunc(x, x)
 	}
-}
-
-func mapInts(f func(int) int, nums []int) []int {
-	result := make([]int, len(nums))
-	for i, v := range nums {
-		result[i] = f(v)
-	}
-	return result
-}
-
-func double(x int) int {
-	return x * 2
 }
