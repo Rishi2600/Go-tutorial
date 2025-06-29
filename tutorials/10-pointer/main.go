bd2cb46af7c3348929f6ffe0f2236cbb54ac711d
Rishi2600 <rishiraj6177@gmail.com>
2025-06-29 21:38:10 +0530

    pointers

---
+++ tutorials/10-pointer/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 5
+	fmt.Println(a)
+
+	var b int = a
+	fmt.Println(b)
+
+	var z *int = &a
+	fmt.Println(z)
+	fmt.Println(*z)
+
+	*z = 9
+	fmt.Println(*z)
+	fmt.Println(a)
+
+}
+
+/*& defines the address and * goes for the pointer, a value of a var can be cahnged by just cahnging the value of the underlying address*/
