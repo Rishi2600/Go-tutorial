55d3bf7415374d1ddc7eeb11671df23c5e137d67
Rishi2600 <rishiraj6177@gmail.com>
2025-07-02 23:32:35 +0530

    receiving the value of struct via pointer

--- tutorials/4-struct/main.go
+++ tutorials/4-struct/main.go
@@ -21,8 +21,22 @@ func main() {
 		phoneNumber: 123456,
 		message:     "you're the guy with phone number",
 	})
+
+	area(&rect{
+		width:  200,
+		height: 6,
+	})
 }
 
 func message(m messageTosend) {
 	fmt.Printf("message sent to phone numbe: %v, & it is %v \n", m.phoneNumber, m.message)
+}
+
+type rect struct {
+	width  int32
+	height int32
+}
+
+func area(r *rect) int32 {
+	return r.height * r.width
 }
