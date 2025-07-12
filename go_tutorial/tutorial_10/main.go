f6c7cf03b28304b54ed5c88de5735adba7600fd5
Rishi2600 <rishiraj6177@gmail.com>
2025-07-12 23:13:14 +0530

    generics updated

--- go_tutorial/tutorial_10/main.go
+++ go_tutorial/tutorial_10/main.go
@@ -3,15 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	var intSlice = []int{1, 2, 3}
-	fmt.Println(isEmpty(intSlice))
+	var intSliceEmpty = []int{1, 2, 3}
+	fmt.Println(isEmpty(intSliceEmpty))
 
-	var floatSlice = []float32{1, 2, 3}
-	fmt.Println(isEmpty(floatSlice))
+	var floatSliceEmpty = []float32{1, 2, 3}
+	fmt.Println(isEmpty(floatSliceEmpty))
+
+	var intSliceNotEmpty = []int{1, 2, 3}
+	fmt.Println(isNotEmpty(intSliceNotEmpty))
+
+	var floatSliceNotEmpty = []float32{1, 2, 3}
+	fmt.Println(isNotEmpty(floatSliceNotEmpty))
 
 }
 
 func isEmpty[T any](slice []T) bool {
 
 	return len(slice) == 0
+}
+
+func isNotEmpty[T any](slice []T) bool {
+	return len(slice) != 0
 }
