8c7dfb281f2dfae324d8bc3c8a5b2e566660b65c
Rishi2600 <rishiraj6177@gmail.com>
2025-07-14 11:32:32 +0530

    channels: looping over vaues of channels

--- tutorials/12-channels/main.go
+++ tutorials/12-channels/main.go
@@ -9,16 +9,16 @@ func main() {
 	c := make(chan int, 5)
 
 	go func() {
-		sum := 0
 		for i := range 10 {
 			fmt.Println("return from the first function: ", i)
-			sum += i
+			c <- i
 		}
-		c <- sum
 	}()
 
 	time.Sleep(time.Millisecond * 1000)
-	output := <-c
 
-	fmt.Println("Output: ", output)
+	for values := range c {
+		fmt.Println(values)
+	}
+
 }
