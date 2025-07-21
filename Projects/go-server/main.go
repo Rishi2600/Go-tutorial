9081a73d188e95ab58eb10256e77bbea6f3e092c
Rishi2600 <rishiraj6177@gmail.com>
2025-07-21 21:38:04 +0530

    simple server logic

--- Projects/go-server/main.go
+++ Projects/go-server/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("listening on port 8080 \n")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
