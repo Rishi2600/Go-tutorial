c5053cfbca3695392add14860ca30ff6b766b8b2
Rishi2600 <rishiraj6177@gmail.com>
2025-07-30 22:38:09 +0530

    .

--- go_tutorial/overall/main.go
+++ go_tutorial/overall/main.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	var array [5]int = [5]int{1, 2, 3, 4, 50}
-	fmt.Println(array)
-	array[0] = 596
-	fmt.Println(array)
+	sum := 0
+
+	for i := range array {
+		sum += i
+	}
+	fmt.Println(sum)
 }
