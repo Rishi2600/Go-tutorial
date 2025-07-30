e5f944390f678b3fb3b3e6b9eaaa990570c0fa08
Rishi2600 <rishiraj6177@gmail.com>
2025-07-30 22:17:51 +0530

    .

--- go_tutorial/overall/main.go
+++ go_tutorial/overall/main.go
@@ -1,22 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	gender := "ttrans"
-
-	switch gender {
-	case "male":
-		fmt.Println("you are male")
-	case "female":
-		fmt.Println("you are female")
-	default:
-		fmt.Println("neither male nor female")
-	}
-
-	sum := 0
-	for i := 0; i < 10; i++ {
-		sum += i
-	}
-	fmt.Println(sum)
+	var array [5]int = [5]int{1, 2, 3, 4, 50}
+	println(array)
 }
