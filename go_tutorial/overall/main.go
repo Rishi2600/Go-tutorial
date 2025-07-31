3968a0ee13ba83cc0b3645c5ec34bd240cd58bcd
Rishi2600 <rishiraj6177@gmail.com>
2025-07-31 20:14:37 +0530

    .

--- go_tutorial/overall/main.go
+++ go_tutorial/overall/main.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	var array [5]int = [5]int{1, 2, 3, 4, 50}
-	sum := 0
+	var slice []int = []int{5, 6, 7, 63, 89}
 
-	for _, value := range array {
-		sum += value
-	}
-	fmt.Println(sum)
+	slice = append(slice, 56)
+
+	fmt.Println(array, slice)
+
 }
