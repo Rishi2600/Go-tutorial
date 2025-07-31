a7a00170eafc8c5c6ae0bea5c76f3d9c270d32b9
Rishi2600 <rishiraj6177@gmail.com>
2025-07-31 20:16:12 +0530

    .

--- go_tutorial/overall/main.go
+++ go_tutorial/overall/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	var array [5]int = [5]int{1, 2, 3, 4, 50}
-	var slice []int = []int{5, 6, 7, 63, 89}
+	var slice []int = make([]int, 5)
 
 	slice = append(slice, 56)
 
