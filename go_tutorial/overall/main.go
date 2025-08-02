4761fd1c849e2b3f7a3e38ed4bacf23619a33ebd
Rishi2600 <rishiraj6177@gmail.com>
2025-08-02 20:37:12 +0530

    .

--- go_tutorial/overall/main.go
+++ go_tutorial/overall/main.go
@@ -5,15 +5,22 @@ import (
 )
 
 func main() {
+
+	sum := 0
+
 	func() {
-		for i := range 1000 {
-			fmt.Println(i)
+		for i := range 3 {
+			sum += i
+			fmt.Println(sum)
 		}
+		fmt.Println(sum)
 	}()
 	func() {
-		for i := range 1000 {
-			fmt.Println(i)
+		for i := range 3 {
+			sum += i
+			fmt.Println(sum)
 		}
+		fmt.Println(sum)
 	}()
 
 	fmt.Println("done")
