ff23d859ddf07b55bad343cdb7007eaa0ca88756
Rishi2600 <rishiraj6177@gmail.com>
2025-08-03 00:29:21 +0530

    .

--- go_tutorial/overall/main.go
+++ go_tutorial/overall/main.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
 
 	ch := make(chan int)
+	var wg sync.WaitGroup
 
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		value := <-ch
 		fmt.Println(value)
 	}()
 
 	ch <- 10
 
-	fmt.Println("done")
-
+	wg.Wait()
 }
