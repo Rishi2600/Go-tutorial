c23dd266c24f3309b561d4a02dbcc6fc919a568a
Rishi2600 <rishiraj6177@gmail.com>
2025-08-02 23:51:48 +0530

    .

--- go_tutorial/overall/main.go
+++ go_tutorial/overall/main.go
@@ -2,28 +2,32 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
-
-var ch = make(chan int)
-
-func expensiveOp(ch chan int) {
-	sum := 0
-	for i := range 10 {
-		sum += i
-	}
-
-	ch <- sum
-}
 
 func main() {
 
-	go expensiveOp(ch)
+	var ch = make(chan int)
 
-	finalSum := <-ch
+	go func() {
+		sum := 0
+		for i := range 50 {
+			sum += i
+		}
+		ch <- sum
+	}()
+	firstSum := <-ch
+	fmt.Println(firstSum)
 
-	fmt.Println(finalSum)
+	go func() {
+		sum := 0
+		for i := 50; i < 101; i++ {
+			sum += i
+		}
+		ch <- sum
+	}()
+	secondSum := <-ch
+	fmt.Println(secondSum)
 
-	time.Sleep(time.Millisecond * 2000)
-	fmt.Println("done")
+	fmt.Println(firstSum + secondSum)
+
 }
