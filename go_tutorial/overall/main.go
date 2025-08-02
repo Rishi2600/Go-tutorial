660a1e1437b54780bdea694a71d4168d55fe8b2e
Rishi2600 <rishiraj6177@gmail.com>
2025-08-02 20:24:41 +0530

    .

--- go_tutorial/overall/main.go
+++ go_tutorial/overall/main.go
@@ -2,22 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	go func() {
+	func() {
 		for i := range 1000 {
 			fmt.Println(i)
 		}
 	}()
-	go func() {
+	func() {
 		for i := range 1000 {
 			fmt.Println(i)
 		}
 	}()
-
-	time.Sleep(time.Second * 2)
 
 	fmt.Println("done")
 }
