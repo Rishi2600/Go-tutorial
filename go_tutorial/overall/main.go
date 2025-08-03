3fcf7bb705cbb41c259b46c450c96d3dc510ebfb
Rishi2600 <rishiraj6177@gmail.com>
2025-08-03 20:15:26 +0530

    .

--- go_tutorial/overall/main.go
+++ go_tutorial/overall/main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
-	ch := make(chan bool)
+	var wg sync.WaitGroup
 
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		sum := 0
 		for i := range 10 {
 			sum += i
 		}
 		fmt.Printf("goroutine working, sum is: %v \n", sum)
-		ch <- true
 	}()
 
-	<-ch
+	wg.Wait()
 	fmt.Println("main thread exit")
 }
+
+/*it's the most ideal way of making sure the goroutine is completed before the main thread exists*/
