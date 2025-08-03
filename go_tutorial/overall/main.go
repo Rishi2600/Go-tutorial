8dc3d89933e69c2028f18b0342767a42d2035821
Rishi2600 <rishiraj6177@gmail.com>
2025-08-03 20:08:37 +0530

    .

--- go_tutorial/overall/main.go
+++ go_tutorial/overall/main.go
@@ -2,23 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 
-	ch := make(chan int, 5)
-
 	go func() {
+		sum := 0
 		for i := range 10 {
-			fmt.Printf("iterated value: %v \n", i)
-			ch <- i
+			sum += i
 		}
+		fmt.Printf("goroutine working, sum is: %v \n", sum)
 	}()
 
-	time.Sleep(time.Second * 2)
-	fmt.Printf("channel value: %v \n", <-ch)
-
-	time.Sleep(time.Second * 5)
-	fmt.Println("DONE")
+	fmt.Println("main thread exit")
 }
