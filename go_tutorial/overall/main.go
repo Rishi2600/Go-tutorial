be735c046d76a9d4aa4e82171845c35e6c316d42
Rishi2600 <rishiraj6177@gmail.com>
2025-08-03 20:24:38 +0530

    sending waitgroup as a functional argument

--- go_tutorial/overall/main.go
+++ go_tutorial/overall/main.go
@@ -5,21 +5,22 @@ import (
 	"sync"
 )
 
+var wg sync.WaitGroup
+
+func expensiveOp(wg *sync.WaitGroup) {
+	defer wg.Done()
+	sum := 0
+	for i := range 10 {
+		sum += i
+	}
+	fmt.Printf("goroutine working, sum is: %v \n", sum)
+}
+
 func main() {
-	var wg sync.WaitGroup
 
 	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		sum := 0
-		for i := range 10 {
-			sum += i
-		}
-		fmt.Printf("goroutine working, sum is: %v \n", sum)
-	}()
+	go expensiveOp(&wg)
 
 	wg.Wait()
 	fmt.Println("main thread exit")
 }
-
-/*it's the most ideal way of making sure the goroutine is completed before the main thread exists*/
