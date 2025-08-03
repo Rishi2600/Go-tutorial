691dbe0e7c6056ef308a2728776ef52c491d06fd
Rishi2600 <rishiraj6177@gmail.com>
2025-08-03 20:10:59 +0530

    .

--- go_tutorial/overall/main.go
+++ go_tutorial/overall/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
+	ch := make(chan bool)
 
 	go func() {
 		sum := 0
@@ -13,8 +13,9 @@ func main() {
 			sum += i
 		}
 		fmt.Printf("goroutine working, sum is: %v \n", sum)
+		ch <- true
 	}()
 
-	time.Sleep(time.Second * 5)
+	<-ch
 	fmt.Println("main thread exit")
 }
