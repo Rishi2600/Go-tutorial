0f1593443bfae3f9a55882de284966ad33a4b877
Rishi2600 <rishiraj6177@gmail.com>
2025-08-05 20:32:38 +0530

    movie struct

--- Projects/go-crud/main.go
+++ Projects/go-crud/main.go
@@ -1,1 +1,11 @@
 package main
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+}
