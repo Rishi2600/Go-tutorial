7114105dd7cfb9795dc7c5e0fbcf79a7bdc5d8e6
Rishi2600 <rishiraj6177@gmail.com>
2025-08-21 19:58:25 +0530

    book route: my approach of creating the book route

--- Projects/BookManage/pkg/route/bookstoreRoutes.go
+++ Projects/BookManage/pkg/route/bookstoreRoutes.go
@@ -1,1 +1,12 @@
 package route
+
+import (
+	"github.com/Rishi2600/Go-tutorial/pkg/controller"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func() {
+	var router *mux.Router = mux.NewRouter()
+
+	router.HandleFunc("/book/", controller.CreateBook).Methods("POST")
+}
