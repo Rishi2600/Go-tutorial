5ddee4265d5ba9a986aa4e6d8a6b04877dcfeb5a
Rishi2600 <rishiraj6177@gmail.com>
2025-09-02 20:32:50 +0530

    cmd/main: func main created

--- Projects/BookManage/cmd/main/main.go
+++ Projects/BookManage/cmd/main/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/Rishi2600/Go-tutorial/pkg/route"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	route.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
