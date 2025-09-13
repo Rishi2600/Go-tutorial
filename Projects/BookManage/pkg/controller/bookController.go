9d1b16c2315350d8c98f7d674dd9dfc0635e9479
Rishi2600 <rishiraj6177@gmail.com>
2025-09-13 22:08:21 +0530

    controllers: GetAllBooks

--- Projects/BookManage/pkg/controller/bookController.go
+++ Projects/BookManage/pkg/controller/bookController.go
@@ -1,1 +1,20 @@
 package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/Rishi2600/Go-tutorial/pkg/models"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+	fmt.Printf("Books: %v", res)
+}
