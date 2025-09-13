0fbc5c1668c6325b18641d4391424960bfd9690a
Rishi2600 <rishiraj6177@gmail.com>
2025-09-13 22:23:28 +0530

    controllers: GetBookById

--- Projects/BookManage/pkg/controller/bookController.go
+++ Projects/BookManage/pkg/controller/bookController.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/Rishi2600/Go-tutorial/pkg/models"
+	"github.com/gorilla/mux"
 )
 
 var NewBook models.Book
@@ -17,4 +19,21 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 	fmt.Printf("Books: %v", res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+
+	if err != nil {
+		fmt.Println("error while parsing the request body.")
+	}
+
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+	fmt.Printf("Book: %v", res)
 }
