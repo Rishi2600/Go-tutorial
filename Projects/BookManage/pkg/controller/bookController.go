4544d17e8359da0ee28321902811d45ba55b7e3f
Rishi2600 <rishiraj6177@gmail.com>
2025-09-13 22:49:23 +0530

    controllers: CreateBook

--- Projects/BookManage/pkg/controller/bookController.go
+++ Projects/BookManage/pkg/controller/bookController.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/Rishi2600/Go-tutorial/pkg/models"
+	"github.com/Rishi2600/Go-tutorial/pkg/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -36,4 +37,32 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 	fmt.Printf("Book: %v", res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+	fmt.Printf("the created book: %v", res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+
+	if err != nil {
+		fmt.Println("error while parsing the request body.")
+	}
+
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+	fmt.Printf("the deleted book: %v", res)
 }
